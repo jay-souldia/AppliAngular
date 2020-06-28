b2268e3e5ebb784fc93ee514ed6f68af6ec44821
souleymane Diallo <dialau85@yahoo.fr>
2020-06-28 02:58:27 +0200

    creation structure projet

---
+++ bi-nails/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ProductsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
